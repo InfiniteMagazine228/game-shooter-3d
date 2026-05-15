2df0ded64b512ace8a7cf75199270f31095a0670
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-15 18:42:42 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Phục vụ tất cả các file tĩnh trong thư mục hiện tại
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server đang chạy tại: http://localhost:${PORT}`);
+});
